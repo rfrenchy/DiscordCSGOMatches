cd56e903e98731ea89079c9dd48bf0f4b02283ca
Ryan French <ryanfrench@outlook.com>
2019-09-23 23:02:23 +0100

    Move MatchSpec dependencies to same file to fix test on build server

--- src/Matches/MatchService/MatchService.spec.ts
+++ src/Matches/MatchService/MatchService.spec.ts
@@ -1,7 +1,72 @@
-import { CreateMockUpcomingMatch, CreateMockFullMatch } from "../../Commands/upcoming/MockUpcomingData";
 import { MatchService } from "./MatchService";
 import { HLTV } from "hltv";
-import { TEAM_NAMES } from "../../../test/Common/MockTeamNames";
+
+import UpcomingMatch from "hltv/lib/models/UpcomingMatch";
+import FullMatch from "hltv/lib/models/FullMatch";
+import MapSlug from "hltv/lib/enums/MapSlug";
+
+const MOCK_DEFAULT_UPCOMING_MATCH: UpcomingMatch = {
+	id: 12335,
+	stars: 2,
+	live: false
+};
+
+const TEAM_NAMES = {
+	1: "Cloud9",
+	2: "Faze",
+	3: "NRG",
+	4: "Complexity",
+	5: "Astralis",
+	6: "Liquid",
+	7: "NaVi",
+	8: "Avangar",
+	9: "NiP",
+	10: "Tyloo",
+	11: "Fnatic",
+	12: "Ghost",
+	13: "Renegades",
+	14: "Envyus",
+	15: "G2",
+	16: "Vitality",
+	17: "Mibr",
+	18: "ENCE",
+	19: "BIG",
+	20: "Virtus Pro"
+};
+
+const CreateMockUpcomingMatch = (options?: Partial<UpcomingMatch>): UpcomingMatch => {
+	return Object.assign(
+		{},
+		{
+			...{
+				id: 12335,
+				stars: 2,
+				live: false
+			},
+			...options
+		}
+	);
+};
+
+const CreateMockFullMatch = (options?: Partial<FullMatch>): FullMatch => {
+	return Object.assign(
+		{},
+		{
+			...{
+				...MOCK_DEFAULT_UPCOMING_MATCH,
+				additionalInfo: "",
+				maps: [{ name: MapSlug.Mirage, result: "win for Faze" }],
+				streams: [],
+				demos: [],
+				hasScorebot: false,
+				date: 1550408400000,
+				format: "",
+				event: { name: "" }
+			},
+			...options
+		}
+	);
+};
 
 describe("The MatchService", () => {
 	const matchService = new MatchService();
